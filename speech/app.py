f421e53ee4c31e68d585414043c5a3cb76770733
LouayLH10 <hcinlouay@gmail.com>
2026-08-20 20:01:01 +0100

    AIvoice

--- speech/app.py
+++ speech/app.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
      #   host="127.0.0.1",
       #  port=5000
    # )
-     app.run(
+    app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=5000
     )
